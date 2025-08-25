a69335b21e24f1b7a31c72fdb7559e092294564c
hxueh <hxueh@users.noreply.github.com>
2025-08-25 10:34:12 +0800

    Update autocmds.lua

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Update lazy.nvim plugins on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("autoupdate", { clear = true }),
+  callback = function()
+    require("lazy").update({ show = false })
+  end,
+})
